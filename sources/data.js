aedfa108870333cd6d8deb2e6cf1702250ae0be6
Evan <rideburtonsnow3@gmail.com>
2018-05-01 21:03:00 -0600

    adding code to register.js

--- sources/data.js
+++ sources/data.js
@@ -1,5 +1,12 @@
 module.exports = {
     firstName: 'Evan',
     lastName: 'Wright',
-     
+    address: '1288 W 1150 N',
+    city: 'Layton',
+    state: 'UT',
+    zipCode: '84041',
+    phoneNumber: '555-555-5555',
+    email: 'evantesting@gmail.com',
+    reTypeEmail: 'evantesting@gmail.com',
+
 }
