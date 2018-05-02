862dd9272b6c1c55f24c145a0ea7bbcc08d4d220
Evan <rideburtonsnow3@gmail.com>
2018-05-01 21:39:42 -0600

    added more code

--- pageObjects/register.js
+++ pageObjects/register.js
@@ -1,12 +1,20 @@
 module.exports = {
     url: 'http://138.68.58.73:5050/',
     elements: {
+        addressInput: 'input[placeholder="Address"]',
+        cityInput: 'input[placeholder="City"]',
+        emailInput: 'input[placeholder="Email (e.g. email@gmail.com)"]',
+        firstNameInput:'input[placeholder="First Name"]', 
+        lastNameInput: 'input[placeholder="Last Name"]',
+        phoneInput: 'input[placeholder="Phone Number"]',
         registerButton: 'a[href="/register"]',
         registerTitle: {
             selector: '//h1[.="Register"]',
             locateStrategy: 'xpath'
         },
-        
+        reTypeEmailInput: 'input[placeholder="Re-Type Email"]',
+        stateInput: 'input[placeholder="State"]',
+        zipCodeInput: 'input[placeholder="Zip Code"]',
 
     }
 }
--- sources/functions.js
+++ sources/functions.js
@@ -5,6 +5,15 @@ const registerForm = (brwoser, data) => {
         .waitForElementVisible('@registerButton', 5000)
         .click('@registerButton')
         .waitForElementPresent('@registerTitle', 5000)
+        .setValue('@firstNameInput', data.firstName)
+        .setValue('@lastNameInput', data.lastName)
+        .setValue('@addressInput', data.address)
+        .setValue('@cityInput', data.city)
+        .setValue('@stateInput', data.state)
+        .setValue('@zipCodeInput', data.zipCode)
+        .setValue('@phoneInput', data.phoneNumber)
+        .setValue('@emailInput', data.email)
+        .setValue('@reTypeEmailInput', data.reTypeEmail)
 
 }
 
