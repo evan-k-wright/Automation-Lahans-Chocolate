7eb38ef5244604ea40e7c0a1ca356e140bbe5862
Evan <rideburtonsnow3@gmail.com>
2018-05-07 19:16:42 -0600

    cleaning up code

--- pageObjects/products.js
+++ pageObjects/products.js
@@ -2,41 +2,35 @@ module.exports = {
     url: 'http://138.68.58.73:5050/',
     elements: {
 
-        //xpath
         boxedChocolates: {
             selector: '//h1[.="Boxed Chocolates"]',
             locateStrategy: 'xpath'
         },
 
-        //path
         halfPoundBox:{
             selector: '//h1[.="Half Pound Box"]',
             locateStrategy: 'xpath'
         },
         halfPound: 'a[href="/halfpound/9"]',
 
-        //xpath
         lePetiteBox: {
             selector: '//h1[.="Le Petite Box"]',
             locateStrategy: 'xpath'
         },
         lePetite: 'a[href="/lepetite/8"]',
 
-        //xpath
         onePoundBox: {
             selector: '//h1[.="One Pound Box"]',
             locateStrategy: 'xpath'
         },
         onePound: 'a[href="/onepound/10"]',
 
-        //xpath
         twoPoundBox: {
             selector: '//h1[.="Two Pound Box"]',
             locateStrategy: 'xpath'
         },
         twoPound: 'a[href="/twopound/11"]',
 
-        //xpath
         productButton: {
             selector: '(//a[@href="/products"])[1]',
             locateStrategy: 'xpath'
